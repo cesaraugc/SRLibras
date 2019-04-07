27136e42d142b6fba2a4ff565cc6507ee98fc343
Cesar Carvalho <cesaraugc@gmail.com>
2019-04-07 15:55:44 -0300

    Insere prints via bluetooth e separa print

--- obtain-smu6050-values/obtain-smu6050-values.ino
+++ obtain-smu6050-values/obtain-smu6050-values.ino
@@ -12,11 +12,13 @@ int16_t gx, gy, gz;
 const int MPU=0x69;  // I2C address of the MPU-6050 This is valid only when AD0 is HIGH
 
 
-// SoftwareSerial bluetooth(2, 3); //TX, RX (Bluetooth)
+SoftwareSerial bluetooth(5, 6); //TX, RX (Bluetooth)
 
 void setup() {
 
   Serial.begin(9600);
+  bluetooth.begin(9600);  
+
   pinMode(LED_PIN, OUTPUT);
   pinMode(BIT0, OUTPUT);
   pinMode(BIT1, OUTPUT);
@@ -32,9 +34,6 @@ void setup() {
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   delay(1000);
-  Serial.println("SETUP");
-
-  // bluetooth.begin(9600);  
 }
 
 void loop() {
@@ -42,35 +41,10 @@ void loop() {
   for(int i=0; i<5; i++){
     setSensor(i);
     delay(10);
-    readAccele();
+    readData();
+    printData();
     delay(1000);
   }; 
-  
-    //MPU6050 accelgyro(0x69);
-  
-    //Serial.println("Initializing I2C devices...");
-    //accelgyro.initialize();
-    
-    // read raw accel/gyro measurements from device
-    //Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-    //accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    // delay(5);
-    // Serial.print(i); Serial.print("Acel. X = "); Serial.print(ax);
-    // // bluetooth.print("Acel. X = "); bluetooth.print(ax);
-    // Serial.print(" | Y = "); Serial.print(ay);
-    // // bluetooth.print(" | Y = "); bluetooth.print(ay);
-    // Serial.print(" | Z = "); Serial.print(az);
-    // // bluetooth.print(" | Z = "); bluetooth.print(az);
-    // Serial.print(" | Gir. X = "); Serial.print(gx);
-    // // bluetooth.print(" | Gir. X = "); bluetooth.print(gx);
-    // Serial.print(" | Y = "); Serial.print(gy);
-    // // bluetooth.print(" | Y = "); bluetooth.print(gy);
-    // Serial.print(" | Z = "); Serial.println(gz);
-    // // bluetooth.print(" | Z = "); bluetooth.println(gz);
-    // Serial.println();
-
-    // //delay(500);
 }
 
 void setSensor(int num){
@@ -78,6 +52,7 @@ void setSensor(int num){
   if(num == 0){
     digitalWrite(INH, HIGH);
     Serial.println("Setando 0");
+    bluetooth.println("Setando 0");
     digitalWrite(BIT0, LOW);
     digitalWrite(BIT1, LOW);
     digitalWrite(BIT2, LOW);
@@ -86,6 +61,7 @@ void setSensor(int num){
   else if(num == 1){
     digitalWrite(INH, HIGH);
     Serial.println("Setando 1");
+    bluetooth.println("Setando 1");
     digitalWrite(BIT0, HIGH);
     digitalWrite(BIT1, LOW);
     digitalWrite(BIT2, LOW);
@@ -94,6 +70,7 @@ void setSensor(int num){
   else if(num == 2){
     digitalWrite(INH, HIGH);
     Serial.println("Setando 2");
+    bluetooth.println("Setando 2");
     digitalWrite(BIT0, LOW);
     digitalWrite(BIT1, HIGH);
     digitalWrite(BIT2, LOW);
@@ -102,6 +79,7 @@ void setSensor(int num){
   else if(num == 3){
     digitalWrite(INH, HIGH);
     Serial.println("Setando 3");
+    bluetooth.println("Setando 3");
     digitalWrite(BIT0, HIGH);
     digitalWrite(BIT1, HIGH);
     digitalWrite(BIT2, LOW);
@@ -110,6 +88,7 @@ void setSensor(int num){
   else if(num == 4){
     digitalWrite(INH, HIGH);
     Serial.println("Setando 4");
+    bluetooth.println("Setando 4");
     digitalWrite(BIT0, LOW);
     digitalWrite(BIT1, LOW);
     digitalWrite(BIT2, HIGH);
@@ -117,27 +96,35 @@ void setSensor(int num){
   }
 }
 
-void readAccele()
-{
-    Wire.beginTransmission(MPU);    // I2C address code thanks to John Boxall
-    Wire.write(0x3B);               // starting with register 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
-    ax=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-    ay=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    az=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    gx=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    gy=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    gz=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-    Wire.endTransmission(true);
-    Serial.print("AcX = "); Serial.print(ax);
-    Serial.print(" | AcY = "); Serial.print(ay);
-    Serial.print(" | AcZ = "); Serial.print(az);
-    //Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-    Serial.print(" | GyX = "); Serial.print(gx);
-    Serial.print(" | GyY = "); Serial.print(gy);
-    Serial.print(" | GyZ = "); Serial.println(gz);
-    Serial.println();
-    delay(5);
+void readData(){
+  Wire.beginTransmission(MPU);    // I2C address code thanks to John Boxall
+  Wire.write(0x3B);               // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+  ax=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  ay=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  az=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  gx=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  gy=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  gz=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Wire.endTransmission(true);
+}
+
+void printData(){
+  Serial.print("AcX = "); Serial.print(ax);
+  bluetooth.print("Acel. X = "); bluetooth.print(ax);
+  Serial.print(" | AcY = "); Serial.print(ay);
+  bluetooth.print(" | Y = "); bluetooth.print(ay);
+  Serial.print(" | AcZ = "); Serial.print(az);
+  bluetooth.print(" | Z = "); bluetooth.print(az);
+  //Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(gx);
+  bluetooth.print(" | Gir. X = "); bluetooth.print(gx);
+  Serial.print(" | GyY = "); Serial.print(gy);
+  bluetooth.print(" | Y = "); bluetooth.print(gy);
+  Serial.print(" | GyZ = "); Serial.println(gz);
+  bluetooth.print(" | Z = "); bluetooth.println(gz);
+  Serial.println();
+  bluetooth.println();
 }
